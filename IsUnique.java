b6811436f948336e9c817c48d3d3af8f1ac85818
Amanda <amandahelenaalfaro@gmail.com>
2016-08-07 10:07:34 -0500

    Add files via upload

---
+++ IsUnique.java
@@ -0,0 +1,51 @@
+package isunique;
+import java.util.*;
+/**
+ *
+ * File Name: isUnique.java
+ * Description: Implement an algorithm to determine if a string has all unique characters
+ * Reference: Chapter 1 Cracking the Coding Interview 6th Edition 
+ * Author: Amanda Alfaro
+ * 
+ */
+public class IsUnique {
+
+    static String helloString; //creating string
+    static Hashtable hashUnique = new Hashtable(); //creating hashtable
+   
+    /**
+    Checks to see if hashtable contains the character in the string
+    if the hashtable "hashUnique" contains the character value 
+    at the specified index of helloString, then the character of 
+    the string is in the hashtable already meaning the string does 
+    not contain unique characters
+    .charAt(i): Returns the char value at the specified index.
+    */
+    
+    public static boolean checkingUnique(){
+        for(int i = 0; i < helloString.length(); i++){
+            //if the hashtable "hashUnique" contains the character value 
+            //at the specified index of helloString, then the character of 
+            //the string is in the hashtable already meaning the string does 
+            //not contain unique characters
+            //.charAt(i): Returns the char value at the specified index.
+            if(hashUnique.contains(helloString.charAt(i))){
+                System.out.println(helloString + " does not contain unique charatcers. ");
+                return false;
+            }
+            else{
+                // putting the character that is not currently in the hashtable into the hashtable
+                hashUnique.put(i, helloString.charAt(i)); //(key, value) parameters: key - the hashtable; key value - the value individual characters in string 
+            }
+        }
+        System.out.println(helloString + " contains unique charatcers. "); 
+        return true;
+    }
+    public static void main(String[] args) {        
+        Scanner cin = new Scanner(System.in); //takes in user input
+        helloString = cin.next(); //set string equal to the next string
+        cin.close();
+       checkingUnique();
+    }
+    
+}
