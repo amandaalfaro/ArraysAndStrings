ff5f438b8273cfabeb65479bf851331ade1cffcb
Amanda <amandahelenaalfaro@gmail.com>
2016-08-11 00:52:09 -0500

    adding header comments

--- URLify/URLify.java
+++ URLify/URLify.java
@@ -1,1 +1,19 @@
+package urlify;
 
+/**
+ *
+ * File Name: URLify.java
+ * Description: Write a method to replace all spaces in a string with '%20' You may assume thst the string hasd sufficent space at the end to hold the additional characters, and that you are given the "true" legth of the string 
+ * Note: When implementing in java, please use a character array so that you can perform this operation in place
+ * Reference: Chapter 1 Cracking the Coding Interview 6th Edition 
+ * Author: Amanda Alfaro
+ * 
+ */
+
+public class URLify {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
