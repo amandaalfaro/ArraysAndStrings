a90ff413ae5fae31794a4fd6ce74d8c8906df3af
amandaalfaro <amanda1025@Amandas-MacBook-Pro.local>
2016-08-11 19:37:15 -0500

    Working

--- URLify/URLify.java
+++ URLify/URLify.java
@@ -1,9 +1,9 @@
 package urlify;
-
+import java.util.*;
 /**
  *
  * File Name: URLify.java
- * Description: Write a method to replace all spaces in a string with '%20' You may assume thst the string hasd sufficent space at the end to hold the additional characters, and that you are given the "true" legth of the string 
+ * Description: Write a method to replace all spaces in a string with '%20' You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given the "true" length of the string 
  * Note: When implementing in java, please use a character array so that you can perform this operation in place
  * Reference: Chapter 1 Cracking the Coding Interview 6th Edition 
  * Author: Amanda Alfaro
@@ -11,9 +11,41 @@ package urlify;
  */
 
 public class URLify {
+    static String charString;
+    static int totalCharacters;
+    
+    public static String replaceSpace(){
+    /**
+    * 
+    * Determines if the specified character is white space according to Java
+    * ch - the character to be tested
+    * Returns:true if the character is a Java whitespace character; false otherwise
+    * 
+    */
+        String newString = ""; 
 
-    public static void main(String[] args) {
-        // TODO code application logic here
+        for(int i = 0; i < totalCharacters; i++){ //start from the last character in the true length of the string
+            if(Character.isWhitespace(charString.charAt(i))){
+                newString += "%20";
+            }else{
+                newString += charString.charAt(i); //add to new string
+            }
+        }
+        return newString;
     }
     
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        
+        System.out.print("Enter a string ");
+        charString = cin.nextLine();
+        
+        System.out.print("How many characters are in that string? ");
+        totalCharacters = cin.nextInt();
+
+        cin.close();
+        
+        System.out.println(replaceSpace());
+        
+    }  
 }
